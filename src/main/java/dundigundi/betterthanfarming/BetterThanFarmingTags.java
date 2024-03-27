6e8182902a413ba79a88d73bbe373d1ae0be7345
bdmajora <bdmajora@protonmail.com>
2024-03-27 10:45:39 -0600

    Fixed

--- src/main/java/dundigundi/betterthanfarming/BetterThanFarmingTags.java
+++
@@ -1,8 +0,0 @@
-package dundigundi.betterthanfarming;
-
-import net.minecraft.core.block.Block;
-import net.minecraft.core.data.tag.Tag;
-
-public class BetterThanFarmingTags {
-	public static final Tag<Block> CUTTABLE_BY_KNIFE = Tag.of("cuttable_by_knife");
-}
--- src/main/java/dundigundi/betterthanfarming/interfaces/IChicken.java
+++
@@ -1,9 +0,0 @@
-package dundigundi.betterthanfarming.interfaces;
-
-import net.minecraft.core.entity.animal.EntityAnimal;
-import net.minecraft.core.entity.animal.EntityChicken;
-import net.minecraft.core.world.World;
-
-public interface IChicken {
-	public void dropFewItems();
-}
